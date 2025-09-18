c6b8ccf67b96df3e9bd733ce07cdc7c5fa6e6140
RAFAELLA MARIA TOSCANO REIS <24122319@CFP127ALUNO.LOCAL>
2025-09-18 15:27:29 -0300

    Tela Home OK - falta o icone do tipo sanguineo

--- app/src/main/java/com/example/doevida/screens/TelaHome.kt
+++ app/src/main/java/com/example/doevida/screens/TelaHome.kt
@@ -1,171 +1,272 @@
 package com.example.doevida.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Newspaper
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.material3.BottomAppBarDefaults.containerColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.doevida.R
 
 @Composable
 fun TelaHome(modifier: Modifier = Modifier) {
+
+    var navController = rememberNavController()
+
     Scaffold(
         bottomBar = {
-            NavigationBar {
-                //mudar a cor
-                NavigationBarItem(
-                    selected = true,
-                    onClick = { },
-                    icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-                    label = { Text("Home") }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    onClick = { },
-                    icon = { Icon(Icons.Default.List, contentDescription = "Notícias") },
-                    label = { Text("Notícias") }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    onClick = { },
-                    icon = { Icon(Icons.Default.Person, contentDescription = "Profile") },
-                    label = { Text("Profile") }
-                )
-            }
+            BarraDeNavegacao(navController)
         }
     ) { paddingValues ->
         Column(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxSize()
                 .background(Color.White)
-                .padding(paddingValues) // garante que conteúdo não fique atrás do bottomBar
+                .padding(paddingValues)
         ) {
-
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-        ) {
-            Box(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFF990410))
-                    .padding(16.dp)
+                    .fillMaxSize()
+                    .background(Color.White)
             ) {
-                Column {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(55.dp)
-                                .clip(CircleShape)
-                                .background(Color.White)
-                        )
-
-                        Spacer(modifier = Modifier.width(12.dp))
-
-                        Text(
-                            text = "Nome do User",
-                            color = Color.White,
-                            fontSize = 15.sp
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        //botão de quantos dias faltam para a próxima doação
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 16.dp),
-                            contentAlignment = Alignment.Center
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFF990410))
+                        .padding(16.dp)
+                ) {
+                    Column {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.spacedBy(12.dp) //espaço entre eles
+                            Box(
+                                modifier = Modifier
+                                    .size(55.dp)
+                                    .clip(CircleShape)
+                                    .background(Color.White)
+                            )
+
+                            Spacer(modifier = Modifier.width(12.dp))
+
+                            Text(
+                                text = "Nome do User",
+                                color = Color.White,
+                                fontSize = 15.sp
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            //botão de quantos dias faltam para a próxima doação
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 16.dp),
+                                contentAlignment = Alignment.Center
                             ) {
-                                Box(
-                                    modifier = Modifier
-                                        .size(70.dp)
-                                        .border(
-                                            width = 3.dp,
-                                            color = Color.White,
-                                            shape = CircleShape
-                                        )
-                                        .clip(CircleShape),
-                                    contentAlignment = Alignment.Center
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.spacedBy(12.dp) //espaço entre eles
                                 ) {
-                                    //mudar depois
+                                    Box(
+                                        modifier = Modifier
+                                            .size(70.dp)
+                                            .border(
+                                                width = 3.dp,
+                                                color = Color.White,
+                                                shape = CircleShape
+                                            )
+                                            .clip(CircleShape),
+                                        contentAlignment = Alignment.Center
+                                    ) {
+                                        //mudar depois
+                                        Text(
+                                            text = "13",
+                                            color = Color.White,
+                                            fontSize = 36.sp,
+                                            fontWeight = FontWeight.Bold
+                                        )
+                                    }
                                     Text(
-                                        text = "13",
+                                        text = "Dias restantes para\na próxima doação",
                                         color = Color.White,
-                                        fontSize = 36.sp,
-                                        fontWeight = FontWeight.Bold
+                                        fontSize = 16.sp
                                     )
                                 }
-                                Text(
-                                    text = "Dias restantes para\na próxima doação",
-                                    color = Color.White,
-                                    fontSize = 16.sp
-                                )
                             }
                         }
                     }
                 }
+
+                Spacer(modifier = Modifier.height(50.dp))
+
+                //barra de pesquisa
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = { Text("Find Seekers") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search",
+                            tint = Color(0xFF990410)
+                        )
+                    },
+                    shape = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+
+
+                Spacer(modifier = Modifier.height(50.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        CardButton("Doar Sangue", R.drawable.doarsangue)
+                        CardButton("Hospitais", R.drawable.hospitais)
+                    }
+                        Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                            CardButton("Banco de Sangue", R.drawable.bancodesangue)
+                            CardButton("Histórico", R.drawable.historicos)
+                    }
+                    CardButton("Registrar Doação", R.drawable.doarsangue)
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
             }
-
-            Spacer(modifier = Modifier.height(50.dp))
-
-            //barra de pesquisa
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                placeholder = { Text("Find Seekers") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "Search",
-                        tint = Color(0xFF990410)
-                    )
-                },
-                shape = RoundedCornerShape(24.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            )
-
-
-            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
+//-------------------------------------
+@Composable
+fun CardButton(text: String, imageRes: Int) {
+        Card(
+            modifier = Modifier
+                .size(width = 150.dp, height = 90.dp)
+                .clickable { /* ação do botão */ },
+            shape = RoundedCornerShape(12.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            border = BorderStroke(1.dp, Color.LightGray)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = text,
+                modifier = Modifier.size(50.dp)
+            )
 
-
-
+            Text(
+                text = text,
+                color = Color(0xFFB71C1C),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+//-------------------------------------
+@Composable
+fun BarraDeNavegacao(navController: NavController?) {
+    NavigationBar(
+        containerColor = Color(0xFF990410) //mudar a cor
+    ) {
+        NavigationBarItem(
+            selected = false,
+            onClick = { navController!!.navigate(route = "tela_home") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            },
+            label = {
+                Text(
+                    text = "Home",
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { //arrumar depois
+                },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Newspaper,
+                    contentDescription = "Notícias",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            },
+            label = {
+                Text(
+                    text = "Notícias",
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {
+            // arrumar depois
+            },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Profile",
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            },
+            label = {
+                Text(
+                    text = "Profile",
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/doevida/ui/theme/Theme.kt
+++ app/src/main/java/com/example/doevida/ui/theme/Theme.kt
@@ -18,9 +18,8 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = primary,
+    onPrimary = onPrimary
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
